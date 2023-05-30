f379a14550d5a2be218811d6455db42ba53403df
unknown <annieyashley@gmail.com>
2023-05-30 17:26:41 -0500

    adds image of deployed site, fixes styling on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ import { FaLinkedin, FaGithub } from "react-icons/fa";
 function Footer(props){
     return(
         <>
-            <a href = "https://www.linkedin.com/in/annie-ashley-902056200/">LinkedIn <FaLinkedin id='linkedinIcon'/></a>
-            <a href = "https://github.com/Annie14254">GitHub <FaGithub id="githubIcon"/></a>
+            <a href = "https://www.linkedin.com/in/annie-ashley-902056200/" className='footerLink'>LinkedIn <FaLinkedin id='linkedinIcon'/></a>
+            <a href = "https://github.com/Annie14254" className='footerLink'>GitHub <FaGithub id="githubIcon"/></a>
         </>
     )
 }
